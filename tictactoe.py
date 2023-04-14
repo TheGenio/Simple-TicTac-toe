576b6798b32bfced73f9892bf4fcc5d90c1a9d2d
TheGenio <90417603+TheGenio@users.noreply.github.com>
2023-04-14 06:32:18 +0300

    Update tictactoe.py
    Found some logic errors and fixed them, also added comments to explain the code more

--- tictactoe.py
+++ tictactoe.py
@@ -1,11 +1,12 @@
+# Set the initial symbol values
 symbols = "XOXOXOXOX"
 
-#this function will check the coordinates given and make sure they are valid
+# This function will check the coordinates given and make sure they are valid
 def true_cord():
     while True:
         try:
-            num_cord = input("").replace(" ", "")
-            if int(num_cord) > 33:
+            num_cord = input("Enter the coordinates: ").replace(" ", "")
+            if int(num_cord) > 33: # Since the input will be converted from 2 1 to 21, thus if the input is higher its wrong. EX 4(there is no 4th row) 1 -> 41
                 print("Coordinates should be from 1 to 3!")
             else:
                 break
@@ -14,23 +15,28 @@ def true_cord():
     return num_cord
 
 
-def gridfun():
+def gridfun(): # Function to print the grid
     print("---------")
     print("|", grid[0][0], grid[0][1], grid[0][2], "|")
     print("|", grid[1][0], grid[1][1], grid[1][2], "|")
     print("|", grid[2][0], grid[2][1], grid[2][2], "|")
     print("---------")
 
+
+# Set the initial values for the game grid and moves
 grid = [[" ", " " , " " ], [" ", " ", " "], [" ", " ", " "]]
 moves = 0
 
-#starts the game
-for player in symbols:
+# Starts the game
+print("Welcome to this simple game of XO, this game works by asking for coordinates and then placing a symbol on that coordinate ")
+print("for example, 3 1, 1 2 etc. Where (ROW)(COLLUMN):")
+
+for player in symbols: # this will choose the turn from symbols each time
     gridfun()
     x = False
     while True: #this will place the X or O in the coordinate
         try:
-            cord = true_cord()
+            cord = true_cord() # Assigns the correct coordinate to cord 
             count = 0
             for i in cord:
                 count += 1
@@ -56,7 +62,7 @@ for player in symbols:
     who_win = ""
     win_num = 0
     win_num_case = 0
-    for i in range(3):
+    for i in range(3): # Its a 3x3 grid
         try:
             if grid[i][0] == grid[i][1] == grid[i][2]: #this will find any winners in the horizontal lines
                 who_win = grid[i][1]
@@ -70,15 +76,13 @@ for player in symbols:
                 who_win = grid[1][1]
                 if who_win == " ":
                     who_win = ""
+            elif grid[0][i] == grid[1][i] == grid[2][i]: #this will find any winners in the vertial lines
+                who_win = grid[1][i]
+                if who_win == " ":
+                    who_win = ""
         except Exception: #IndexError
             pass
 
-
-    if who_win == "": # if its neither, then this will find the winners in  verical lines
-        for y in range(3):
-            if symbols[y] == symbols[y + 3] and symbols[y + 3] == symbols[y + 6]:
-                who_win = symbols[y]
-                win_num += 1
 
     if moves >= 5 and (who_win == "X" or who_win == "O"): # this will check if there are any winners and ends the game is there is any winners
         gridfun()
